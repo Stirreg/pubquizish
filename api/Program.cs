be5510d327a61be5ca6adc4690acec9b8d5ac596
Erik Gerrits <info@stirreg.nl>
2022-04-19 18:22:41 +0200

    Upgraded dependencies. Migrated from .net 5 to 6. Migrated to minimal hosting model in api.

--- api/Program.cs
+++ api/Program.cs
@@ -1,20 +1,124 @@
-using Microsoft.AspNetCore.Hosting;
+using System.Data;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using NetCore.AutoRegisterDi;
+using Pubquizish;
+using Pubquizish.Authentication;
+using Pubquizish.Game.Api.GraphQL;
+using Pubquizish.Game.Infrastructure.EntityFramework;
 
-namespace api
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.RegisterAssemblyPublicNonGenericClasses()
+                .Where(predicate => predicate.Name.EndsWith("Repository"))
+                .AsPublicImplementedInterfaces();
+
+builder.Services.AddDbContext<AuthenticationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+).AddDbContext<GameDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddScoped<IDbConnection>(connection => new SqliteConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthenticationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+
+        options.AuthorizationEndpoint += "?prompt=consent";
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.None;
+
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        return Task.CompletedTask;
+    };
+
+    options.Events.OnRedirectToAccessDenied = context =>
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+        return Task.CompletedTask;
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("https://localhost:3000", "https://localhost:3001");
+        builder.AllowCredentials();
+        builder.AllowAnyHeader();
+    });
+});
+
+builder.Services.AddGraphQLServer()
+    .AddAuthorization()
+    .AddQueryType()
+        .AddTypeExtension<GamesQuery>()
+    .AddMutationType()
+        .AddTypeExtension<CreateNewGameMutation>()
+        .AddTypeExtension<RemoveNewGameMutation>();
+
+builder.Services.AddMediatR(typeof(Program));
+
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+});
+
+builder.Services.AddHealthChecks();
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapHealthChecks("/healthz");
+app.MapControllers();
+app.MapGraphQL();
+
+app.Run();
